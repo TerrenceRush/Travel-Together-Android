d0da3ff7c9b55aaded1446315110a48030d371fa
Hong Guo <Edward@dyn-160-39-143-198.dyn.columbia.edu>
2015-04-15 20:33:24 -0400

    detailed activity

--- app/src/main/java/com/example/xinyue/helloworld/DetailActivity.java
+++ app/src/main/java/com/example/xinyue/helloworld/DetailActivity.java
@@ -1,7 +1,8 @@
 package com.example.xinyue.helloworld;
 
-import android.support.v7.app.ActionBarActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
