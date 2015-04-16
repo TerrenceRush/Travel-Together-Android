a007323c9be442716165d1f8c438b38c1934d4cf
Hong Guo <Edward@dyn-160-39-143-198.dyn.columbia.edu>
2015-04-15 20:37:41 -0400

    add new post activity

--- app/src/main/java/com/example/xinyue/helloworld/NewPostActivity.java
+++ app/src/main/java/com/example/xinyue/helloworld/NewPostActivity.java
@@ -4,10 +4,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 import com.example.xinyue.helloworld.R;
 
@@ -18,6 +24,7 @@ public class NewPostActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
         addListenerOnButton();
+        addListenerOnButton1();
     }
 
 
@@ -54,7 +61,7 @@ public class NewPostActivity extends ActionBarActivity {
             @Override
             public void onClick(View arg0) {
 
-                Intent intent = new Intent(context, ListActivity.class);
+                Intent intent = new Intent(context, DetailActivity.class);
                 startActivity(intent);
 
             }
@@ -62,4 +69,70 @@ public class NewPostActivity extends ActionBarActivity {
         });
 
     }
+
+    public void addListenerOnButton1() {
+        final Context context = this;
+        Button button = (Button) findViewById(R.id.post);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg1) {
+                // get information from EditText
+                final EditText destinationField = (EditText) findViewById(R.id.destination);
+                String destination = destinationField.getText().toString();
+
+                final String TAG = "MyActivity";
+
+                Log.v(TAG, "index=" + destination);
+
+                String test = "test";
+
+                final EditText departureDateField = (EditText) findViewById(R.id.departure_date);
+                String departureDate = departureDateField.getText().toString();
+
+                final EditText sizeField = (EditText) findViewById(R.id.group_size);
+                String size = sizeField.getText().toString();
+
+                // get information from RadioGroup
+
+                /*RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup);
+                int selectedID = rg.getCheckedRadioButtonId();
+
+                RadioButton selectedRadioButton = (RadioButton) findViewById(selectedID);
+                String privacy = selectedRadioButton.getText().toString();*/
+
+                // get information from checkbox
+                final CheckBox responseCheck = (CheckBox) findViewById(R.id.shareFacebook);
+                boolean share = responseCheck.isChecked();
+
+                // get information of traveling members
+                final Spinner spinnerMem =  (Spinner) findViewById(R.id.spinner);
+                String members = spinnerMem.getSelectedItem().toString();
+
+                // get additional information
+                final EditText informationField = (EditText) findViewById(R.id.addtional_information);
+                String information = informationField.getText().toString();
+
+                System.out.print(destination+departureDate+size);
+
+
+                Intent intent = new Intent(context, Welcome.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    public void onRadioButtonClicked(View view) {
+        RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup);
+        int selectedID = rg.getCheckedRadioButtonId();
+
+        RadioButton selectedRadioButton = (RadioButton) findViewById(selectedID);
+        String privacy = selectedRadioButton.getText().toString();
+        String test = "test";
+    }
+
+
+
+
 }
